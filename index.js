7cbc548487723242c35dc78d751ef3281d8c5ae5
Nason-shopback <nason@shopback.com>
2018-08-22 10:43:30 +0800

    change port

--- index.js
+++ index.js
@@ -392,7 +392,7 @@ function handleSticker(message, replyToken) {
 }
 
 // listen on port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3033;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
