184bf40110b67fef922d573d275d216c37dc99cb
Nason-shopback <nason@shopback.com>
2018-12-13 02:07:08 +0800

    solve promise

--- index.js
+++ index.js
@@ -153,9 +153,10 @@ function fillInfo(replyToken, choice) {
 function takeOrder(replyToken,choice ,code) {
 
   return new Promise(done => {
+    console.log('choice: '+choice);
     console.log('reservationInfo[choice]'+reservationInfo[choice]);
     console.log('reservationInfo[choice].person_id'+reservationInfo[choice].person_id);
-    reservationInfo[choice].randInput = code
+    reservationInfo[choice].randInput = code;
     var options = {
       url: 'http://railway.hinet.net/Foreign/common/etno11.jsp',
       method: 'POST',
